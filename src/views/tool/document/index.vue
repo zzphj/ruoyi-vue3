e382382ccc53f2cc005dddeeaccff4a53c4fc4cc
zzp <2739980189@qq.com>
2026-09-24 10:44:54 +0800

    文档

---
+++ src/views/tool/document/index.vue
@@ -0,0 +1,262 @@
+<template>
+    <div class="app-container">
+       <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch">
+          <el-form-item label="文档名称" prop="postName">
+             <el-input
+                v-model="queryParams.postName"
+                placeholder="请输入文档名称"
+                clearable
+                style="width: 200px"
+                @keyup.enter="handleQuery"
+             />
+          </el-form-item>
+          <el-form-item label="状态" prop="status">
+             <el-select v-model="queryParams.status" placeholder="文档状态" clearable style="width: 200px">
+                <el-option
+                   v-for="dict in sys_normal_disable"
+                   :key="dict.value"
+                   :label="dict.label"
+                   :value="dict.value"
+                />
+             </el-select>
+          </el-form-item>
+          <el-form-item>
+             <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+             <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+          </el-form-item>
+       </el-form>
+ 
+       <el-row :gutter="10" class="mb8">
+          <el-col :span="1.5">
+             <el-button
+                type="primary"
+                plain
+                icon="Plus"
+                @click="handleAdd"
+                v-hasPermi="['system:post:add']"
+             >新增</el-button>
+          </el-col>
+          <el-col :span="1.5">
+             <el-button
+                type="success"
+                plain
+                icon="Edit"
+                :disabled="single"
+                @click="handleUpdate"
+                v-hasPermi="['system:post:edit']"
+             >修改</el-button>
+          </el-col>
+          <el-col :span="1.5">
+             <el-button
+                type="danger"
+                plain
+                icon="Delete"
+                :disabled="multiple"
+                @click="handleDelete"
+                v-hasPermi="['system:post:remove']"
+             >删除</el-button>
+          </el-col>
+
+          <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+       </el-row>
+ 
+       <el-table v-loading="loading" :data="postList" @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55" align="center" />
+          <el-table-column label="文档编号" align="center" prop="documentId" />
+          <el-table-column label="文档名称" align="center" prop="documentName" />
+          <el-table-column label="文档内容" align="center" prop="document" />
+          <el-table-column label="状态" align="center" prop="documentStatus">
+             <template #default="scope">
+                <dict-tag :options="sys_normal_disable" :value="scope.row.status" />
+             </template>
+          </el-table-column>
+          <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+             <template #default="scope">
+                <span>{{ parseTime(scope.row.createTime) }}</span>
+             </template>
+          </el-table-column>
+          <el-table-column label="操作" width="180" align="center" class-name="small-padding fixed-width">
+             <template #default="scope">
+                <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:post:edit']">修改</el-button>
+                <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:post:remove']">删除</el-button>
+             </template>
+          </el-table-column>
+       </el-table>
+ 
+       <pagination
+          v-show="total > 0"
+          :total="total"
+          v-model:page="queryParams.pageNum"
+          v-model:limit="queryParams.pageSize"
+          @pagination="getList"
+       />
+ 
+       <!-- 添加或修改文档对话框 -->
+       <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+          <el-form ref="postRef" :model="form" :rules="rules" label-width="80px">
+             <el-form-item label="文档名称" prop="postName">
+                <el-input v-model="form.postName" placeholder="请输入文档名称" />
+             </el-form-item>
+             <el-form-item label="文档状态" prop="status">
+                <el-radio-group v-model="form.status">
+                   <el-radio
+                      v-for="dict in sys_normal_disable"
+                      :key="dict.value"
+                      :value="dict.value"
+                   >{{ dict.label }}</el-radio>
+                </el-radio-group>
+             </el-form-item>
+          </el-form>
+          <template #footer>
+             <div class="dialog-footer">
+                <el-button type="primary" @click="submitForm">确 定</el-button>
+                <el-button @click="cancel">取 消</el-button>
+             </div>
+          </template>
+       </el-dialog>
+    </div>
+ </template>
+ 
+ <script setup lang="ts" name="Document">
+ import { listPost, addPost, delPost, getPost, updatePost } from "@/api/system/post"
+ import type { SysPost, PostQueryParams } from '@/types/api/system/post'
+ 
+ const { proxy } = getCurrentInstance()
+ const { sys_normal_disable } = useDict("sys_normal_disable")
+ 
+ const postList = ref<SysPost[]>([])
+ const open = ref<boolean>(false)
+ const loading = ref<boolean>(true)
+ const showSearch = ref<boolean>(true)
+ const ids = ref<number[]>([])
+ const single = ref<boolean>(true)
+ const multiple = ref<boolean>(true)
+ const total = ref<number>(0)
+ const title = ref<string>("")
+ 
+ const data = reactive({
+   form: {} as SysPost,
+   queryParams: {
+     pageNum: 1,
+     pageSize: 10,
+     postCode: undefined,
+     postName: undefined,
+     status: undefined
+   } as PostQueryParams,
+   rules: {
+     postName: [{ required: true, message: "文档名称不能为空", trigger: "blur" }],
+     postCode: [{ required: true, message: "文档编码不能为空", trigger: "blur" }],
+     postSort: [{ required: true, message: "文档顺序不能为空", trigger: "blur" }],
+   }
+ })
+ 
+ const { queryParams, form, rules } = toRefs(data)
+ 
+ /** 查询文档列表 */
+ function getList() {
+   loading.value = true
+   listPost(queryParams.value).then(response => {
+     postList.value = response.rows
+     total.value = response.total
+     loading.value = false
+   })
+ }
+ 
+ /** 取消按钮 */
+ function cancel() {
+   open.value = false
+   reset()
+ }
+ 
+ /** 表单重置 */
+ function reset() {
+   form.value = {
+     postId: undefined,
+     postCode: undefined,
+     postName: undefined,
+     postSort: 0,
+     status: "0",
+     remark: undefined
+   }
+   proxy.resetForm("postRef")
+ }
+ 
+ /** 搜索按钮操作 */
+ function handleQuery() {
+   queryParams.value.pageNum = 1
+   getList()
+ }
+ 
+ /** 重置按钮操作 */
+ function resetQuery() {
+   proxy.resetForm("queryRef")
+   handleQuery()
+ }
+ 
+ /** 多选框选中数据 */
+ function handleSelectionChange(selection: SysPost[]) {
+   ids.value = selection.map(item => item.postId!)
+   single.value = selection.length != 1
+   multiple.value = !selection.length
+ }
+ 
+ /** 新增按钮操作 */
+ function handleAdd() {
+   reset()
+   open.value = true
+   title.value = "添加文档"
+ }
+ 
+ /** 修改按钮操作 */
+ function handleUpdate(row?: SysPost) {
+   reset()
+   const postId = row?.postId || ids.value[0]
+   getPost(postId).then(response => {
+     form.value = response.data!
+     open.value = true
+     title.value = "修改文档"
+   })
+ }
+ 
+ /** 提交按钮 */
+ function submitForm() {
+   proxy.$refs["postRef"].validate((valid: boolean) => {
+     if (valid) {
+       if (form.value.postId != undefined) {
+         updatePost(form.value).then(() => {
+           proxy.$modal.msgSuccess("修改成功")
+           open.value = false
+           getList()
+         })
+       } else {
+         addPost(form.value).then(() => {
+           proxy.$modal.msgSuccess("新增成功")
+           open.value = false
+           getList()
+         })
+       }
+     }
+   })
+ }
+ 
+ /** 删除按钮操作 */
+ function handleDelete(row?: SysPost) {
+   const postIds = row?.postId || ids.value
+   proxy.$modal.confirm('是否确认删除文档编号为"' + postIds + '"的数据项？').then(function() {
+     return delPost(postIds)
+   }).then(() => {
+     getList()
+     proxy.$modal.msgSuccess("删除成功")
+   }).catch(() => {})
+ }
+ 
+ /** 导出按钮操作 */
+ function handleExport() {
+   proxy.download("system/post/export", {
+     ...queryParams.value
+   }, `post_${new Date().getTime()}.xlsx`)
+ }
+ 
+ getList()
+ </script>
+ 
